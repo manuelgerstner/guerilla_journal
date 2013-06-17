68ea073278803cf83e4aecbba3f471963c625ff4
wsc15 <busser@mytum.de>
2013-06-17 21:19:46 +0200

    -n - added views for listing all articles, a single article, a user's profile page and submission of an article.

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,18 +1,17 @@
 package controllers;
 
-import play.*;
-import play.mvc.*;
+import java.util.List;
 
-import java.util.*;
-
-import models.*;
+import models.Article;
+import play.mvc.Controller;
 
 public class Application extends Controller {
 
-    public static void index() {
-        Article frontPost = Article.find("order by postedAt desc").first();
-        List<Article> olderPosts = Article.find("order by postedAt desc").from(1).fetch(10);
-        render(frontPost, olderPosts);        
-    }
+	public static void index() {
+		Article frontPost = Article.find("order by postedAt desc").first();
+		List<Article> olderPosts = Article.find("order by postedAt desc")
+				.from(1).fetch(10);
+		render(frontPost, olderPosts);
+	}
 
 }
