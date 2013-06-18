dc63376fbc28df1bb02893307c3e78272835f93b
NemoOudeis <fuguido@googlemail.com>
2013-06-18 21:07:18 +0200

    test for heroku

--- guerilla_journal/app/controllers/Application.java
+++ guerilla_journal/app/controllers/Application.java
@@ -22,10 +22,12 @@ public class Application extends Controller {
     );
 
     public static void index() {
-
-        Article frontPost = Article.find("order by postedAt desc").first();
-        List<Article> olderPosts = Article.find("order by postedAt desc").from(1).fetch(10);
-        render(frontPost, olderPosts);
+        User user = getUser();
+        renderText("www.google.de");
+        
+        // Article frontPost = Article.find("order by postedAt desc").first();
+        // List<Article> olderPosts = Article.find("order by postedAt desc").from(1).fetch(10);
+        // render(frontPost, olderPosts);
     }
 
     public static void twitterLogin() {
