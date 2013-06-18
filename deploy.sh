0a54d75dd66475878724d33f888e9bb25cca38e4
NemoOudeis <fuguido@googlemail.com>
2013-06-18 15:29:14 +0200

    added heroku deploy script

---
+++ deploy.sh
@@ -0,0 +1,9 @@
+# Intall git subtree first, see
+# http://engineeredweb.com/blog/how-to-install-git-subtree/
+# Also make sure to add the heroku repo as heroku via
+# 	git remote add heroku [repo url]
+
+# for more info on the heroku with play visit
+# https://devcenter.heroku.com/articles/play
+
+git subtree push --prefix guerilla_journal heroku master
