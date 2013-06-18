dcd7aa77260065915f714c21983d5964c4091662
NemoOudeis <fuguido@googlemail.com>
2013-06-18 15:53:09 +0200

    try to get heroku running

--- guerilla_journal/app/Bootstrap.java
+++ guerilla_journal/app/Bootstrap.java
@@ -5,13 +5,11 @@ import play.test.Fixtures;
 
 @OnApplicationStart
 public class Bootstrap extends Job {
-    private static boolean done = false;
 
     public void doJob() {
         try {
-            if (!Bootstrap.done) {
+            if (Arcticle.count() == 0) {
                 Fixtures.loadModels("articles.yml");
-                Bootstrap.done = true;
             }
         } catch (Exception e) {
             // i don't fucking care...
