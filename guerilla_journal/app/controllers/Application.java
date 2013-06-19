f0ff457edd52e1f14ef294cb9e7802ba1ce9104c
wsc15 <busser@mytum.de>
2013-06-19 11:38:14 +0200

    To make this work, MAKE SURE TO RUN play dependencies --sync AND play eclipsify/netbeansify to import the CRUD module
    Changes:
    - fixed crud module import
    - fixed link on template main.html, Users.profile() wasn't implemented.

--- guerilla_journal/app/controllers/Application.java
+++ guerilla_journal/app/controllers/Application.java
@@ -1,28 +1,24 @@
 package controllers;
 
-import java.net.URLEncoder;
-
-import controller.*;
-import models.*;
-import com.google.gson.*;
 import java.util.List;
-import play.libs.OAuth;
-import play.libs.OAuth.ServiceInfo;
-import play.libs.WS;
-import play.mvc.*;
+
+import models.Article;
+import models.User;
+import play.mvc.Controller;
 
 public class Application extends Controller {
 
-    public static void index() {
-        User user = Users.getUser();
-        user.session = session.getId();
-        user.save();
+	public static void index() {
+		User user = Users.getUser();
+		user.session = session.getId();
+		user.save();
 
-        session.put("loggedin",user.isLoggedIn());
+		session.put("loggedin", user.isLoggedIn());
 
-        Article frontPost = Article.find("order by postedAt desc").first();
-        List<Article> olderPosts = Article.find("order by postedAt desc").from(1).fetch(10);
-        render(frontPost, olderPosts);
-    }    
+		Article frontPost = Article.find("order by postedAt desc").first();
+		List<Article> olderPosts = Article.find("order by postedAt desc")
+				.from(1).fetch(10);
+		render(frontPost, olderPosts);
+	}
 
 }
