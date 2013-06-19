ccbffbeee8219bc20148b5796c9e65d515d08507
wsc15 <busser@mytum.de>
2013-06-19 15:13:58 +0200

    -n code formatting applied, typos corrected

--- app/models/Article.java
+++ app/models/Article.java
@@ -1,79 +1,74 @@
 package models;
 
-import play.*;
-import play.db.jpa.*;
+import java.util.Date;
 
-import javax.persistence.*;
-import java.util.*;
+import javax.persistence.Entity;
+import javax.persistence.Lob;
+
+import play.db.jpa.Model;
 
 @Entity
 public class Article extends Model {
-    public String author;
-    public String title;
-    public String summary;
-    @Lob
-    public String entry;
-    public Date postedAt;
-    public Picture picture;
-  //  public ArrayList <String>tags;
-    
-    
-    public Article(String author, String title, String summary, String entry, Picture picture) {
-            super();
-            this.author = author;
-            this.summary = summary;
-            this.entry = entry;
-            this.postedAt = new Date();
-            this.picture = picture;
-    //	this.tags = tags;
-            create();
-    }
+	public String author;
+	public String title;
+	public String summary;
+	@Lob
+	public String entry;
+	public Date postedAt;
+	public Picture picture;
 
+	// public ArrayList <String>tags;
 
-    public String getAuthor() {
-            return author;
-    }
+	public Article(String author, String title, String summary, String entry,
+			Picture picture) {
+		super();
+		this.author = author;
+		this.summary = summary;
+		this.entry = entry;
+		this.postedAt = new Date();
+		this.picture = picture;
+		// this.tags = tags;
+		create();
+	}
 
+	public String getAuthor() {
+		return author;
+	}
 
-    public void setAuthor(String author) {
-            this.author = author;
-    }
+	public void setAuthor(String author) {
+		this.author = author;
+	}
 
+	public String getSummary() {
+		return summary;
+	}
 
-    public String getSummary() {
-            return summary;
-    }
+	public void setSummary(String summary) {
+		this.summary = summary;
+	}
 
+	public String getEntry() {
+		return entry;
+	}
 
-    public void setSummary(String summary) {
-            this.summary = summary;
-    }
+	public void setEntry(String entry) {
+		this.entry = entry;
+	}
 
+	public Date getPostedAt() {
+		return postedAt;
+	}
 
-    public String getEntry() {
-            return entry;
-    }
+	public void setPostedAt(Date postedAt) {
+		this.postedAt = postedAt;
+	}
 
+	public String getTitle() {
+		return title;
+	}
 
-    public void setEntry(String entry) {
-            this.entry = entry;
-    }
+	public void setTitle(String title) {
+		this.title = title;
+	}
 
-    public Date getPostedAt() {
-        return postedAt;
-    }
-
-    public void setPostedAt(Date postedAt) {
-        this.postedAt = postedAt;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-    
-    
 }
--- app/models/User.java
+++ app/models/User.java
@@ -1,46 +1,46 @@
 package models;
 
 import javax.persistence.Entity;
+
+import play.data.validation.Required;
 import play.db.jpa.Model;
-import play.data.validation.*;
 
 /**
- *
+ * 
  * @author Christine
  */
-@Entity(name="Users")
+@Entity(name = "Users")
 public class User extends Model {
-    // emails are not provided by twitter api, do we really need them? - david
-    // @Required
-    // public String email;
-    @Required
-    public String name;
-    public String screenName;
-    public String token; // oauth
-    public String secret; // oauth
-    public String iconUrl;
-    public String session;
-    
-    public User(String fullname) {
-        this.name = fullname;
-    }
-    public static User findOrCreate(String name) {
-        User user = User.find("name", name).first();
-        if (user == null) {
-            user = new User(name);
-        }
-        return user;
-    }
-    
- 
-    public String toString() {
-        return name + " : "
-            + (screenName == null ? " " : screenName)+" : "
-            + (token == null ? "no twitter" : "twitter ok") +" : "
-            + session; 
-    }
+	// emails are not provided by twitter api, do we really need them? - david
+	// @Required
+	// public String email;
+	@Required
+	public String name;
+	public String screenName;
+	public String token; // oauth
+	public String secret; // oauth
+	public String iconUrl;
+	public String session;
 
-    public boolean isLoggedIn(){
-        return (token == null ? false : true );
-    }
+	public User(String fullname) {
+		this.name = fullname;
+	}
+
+	public static User findOrCreate(String name) {
+		User user = User.find("name", name).first();
+		if (user == null) {
+			user = new User(name);
+		}
+		return user;
+	}
+
+	public String toString() {
+		return name + " : " + (screenName == null ? " " : screenName) + " : "
+				+ (token == null ? "no twitter" : "twitter ok") + " : "
+				+ session;
+	}
+
+	public boolean isLoggedIn() {
+		return (token == null ? false : true);
+	}
 }
