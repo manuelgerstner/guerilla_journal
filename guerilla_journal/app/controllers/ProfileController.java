2d58206e28a274e93e3edd35fa2ff73dad2ff606
manuel_gerstner <manuelgerstner@gmail.com>
2013-06-20 01:43:36 +0200

    Minor changes - author / date on main page - user image on profile page

--- guerilla_journal/app/controllers/ProfileController.java
+++ guerilla_journal/app/controllers/ProfileController.java
@@ -1,10 +1,13 @@
 package controllers;
 
+import models.User;
 import play.mvc.Controller;
 
 public class ProfileController extends Controller {
 	
 	public static void index() {
+		User currentUser = Users.getUser();
+		
 		render();
 	}
 
