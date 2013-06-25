4f1cab8cc18507cfb67594acee02769e417caf5f
Christine <christine.noehmeier@gmail.com>
2013-06-25 14:04:09 +0200

    -n Search via browser-url possible (e.g. localhost:9000/search/FC Bayern).

---
+++ app/controllers/Search.java
@@ -0,0 +1,22 @@
+package controllers;
+
+import java.util.List;
+import models.Article;
+import play.Logger;
+import play.modules.search.Query;
+import play.mvc.Controller;
+
+/**
+ *Search controller for basic fulltext search with lucene.
+ * @author Christine
+ */
+public class Search extends Controller{
+    public static void index(String query) {
+            String request = "author:\"" +query  +"\"  title:\""+query+"\" summary:\"" + query + "\" entry:\"" + query + "\"" ;
+
+            Query q = play.modules.search.Search.search(request, Article.class);
+            List<Article> articles = q.fetch();
+            Logger.info("Found " + articles.size() + " articles.");
+            render(articles);
+    }
+}
